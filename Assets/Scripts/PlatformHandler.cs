1ead41f345b7d604f62b98d1d20aa767471360fb
deleo <deleojur@gmail.com>
2014-01-30 13:48:25 +0100

    Please just do it

---
+++ Assets/Scripts/PlatformHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformHandler : MonoBehaviour {
+
+	// Use this for initialization
+	public void FoldIn () {
+		transform.GetComponent<Animator>().Play("FOLDIN");
+        transform.parent.GetComponent<BoxCollider>().enabled = false;
+        //transform.position = new Vector3(transform.position.x, transform.position.y, 5);
+
+	}
+
+    public void FoldOut()
+    {
+        transform.GetComponent<Animator>().Play("FOLDOUT");
+		transform.parent.GetComponent<BoxCollider>().enabled = true;
+       // transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+}
